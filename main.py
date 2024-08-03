bc90272f4e34b9f0e9229337ee789e73f8d01c19
terryproctor <terryproctor2015@outlook.com>
2024-08-03 21:48:33 +0100

    detect walls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 g_snake = Snake()
+g_food = Food()
 
 screen.listen()
 screen.onkey(g_snake.up, "Up")
@@ -20,6 +22,16 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    g_snake.move()      
+    g_snake.move()    
+
+    # Detect collision with food
+    # if head collides with food
+    if g_snake.segments[0].distance(g_food) < 18:
+        g_food.refresh()
+        #g_snake.extend()  
+
+    if abs(g_snake.segments[0].xcor()) > 280 or abs(g_snake.segments[0].ycor()) > 280:
+        game_is_on = False
+        print("Game Over")
 
 screen.exitonclick()
